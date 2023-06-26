29226f3ad7218f9d8568c2490715b25ce31b018d
albim72 <marcin.albiniak72@gmail.com>
2023-06-26 12:28:44 +0200

    Update funkcje_anonimowe.py

--- DZIEN_1/funkcje_anonimowe.py
+++ DZIEN_1/funkcje_anonimowe.py
@@ -13,3 +13,21 @@ def multi(n):
     return lambda a:a*n
 
 print(multi(8)(6))
+
+liczba = [3,12,5,88,9,14,117,-4,0,-24,13,15,1,999,2]
+
+parz = list(filter(lambda x:x%2==0,liczba))
+print(parz)
+
+cube = list(map(lambda x:x**3,liczba))
+print(cube)
+
+cubeset = set(map(lambda x:x**3,liczba))
+print(cubeset)
+
+def fiveplus(x):
+    return x+5
+
+five = list(map(fiveplus,liczba))
+print(five)
+ 
