027771158a1ad28708f0d184a0064eda16a47ac1
albim72 <marcin.albiniak72@gmail.com>
2023-06-26 10:30:06 +0200

    Create kolekcje.py

---
+++ DZIEN_1/kolekcje.py
@@ -0,0 +1,19 @@
+print("kolekcje języka Python")
+a = 55
+print(a)
+print(type(a))
+
+a = "zielone"
+
+print(a)
+print(type(a))
+
+b:float = 9.567
+print(b)
+print(type(b))
+
+b = True
+print(b)
+print(type(b))
+
+imiona = ["Jan","Monika","Leon","Olaf","Halina","Jan","Nadia","Maria","Leon"]
