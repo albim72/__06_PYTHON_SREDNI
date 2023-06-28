a08bc385abc7034f76d761b982247c44fb913fd9
albim72 <marcin.albiniak72@gmail.com>
2023-06-28 13:52:47 +0200

    Update main.py

--- DZIEN_3/RESISTORS/main.py
+++ DZIEN_3/RESISTORS/main.py
@@ -1,4 +1,5 @@
 from oldresistor import OldResistor
+from resistor import Resistor
 
 
 print("__________________________________________")
@@ -8,3 +9,15 @@ print(r0)
 print(r0.get_ohms())
 r0.set_ohms(3.33E3)
 print(r0.get_ohms())
+
+r1 = Resistor(50E3)
+print(f"_______________ klasa {r1.__class__.__name__} ________________")
+
+print(r1.ohms)
+r1.ohms = 5.5E3
+print(r1.ohms)
+
+print(f'układ elektryczny:\n'
+      f'oporność: {r1.ohms} om\n'
+      f'napięcie prądu: {r1.voltage} V\n'
+      f'natężenie prądu: {r1.current} A\n')
