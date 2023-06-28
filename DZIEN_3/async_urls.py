a3079140496d94ce63e9a216cd91c6b40b0dd7c9
albim72 <marcin.albiniak72@gmail.com>
2023-06-28 15:12:41 +0200

    Update async_urls.py

--- DZIEN_3/async_urls.py
+++ DZIEN_3/async_urls.py
@@ -9,3 +9,8 @@ URLS = [
     "https://www.gov.pl/web/obrona-narodowa",
     "https://mil.ru/"
 ]
+
+def load_url(url,timeout):
+    with urllib.request.urlopen(url,timeout=timeout) as conn:
+        return conn.read()
+    
