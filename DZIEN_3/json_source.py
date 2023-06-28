e11acfc359d44b4becaa46a1e60463abf646a841
albim72 <marcin.albiniak72@gmail.com>
2023-06-28 09:58:51 +0200

    Update json_source.py

--- DZIEN_3/json_source.py
+++ DZIEN_3/json_source.py
@@ -8,3 +8,24 @@ osoba = json.loads(jsource)
 print(osoba)
 print(type(osoba))
 print(osoba["nazwisko"])
+
+auto = {
+    "marka":"Jeep",
+    "model":"Cherokee",
+    "rocznik":2020,
+    "poj":4.8
+}
+
+print(auto)
+jsonauto = json.dumps(auto,indent=4)
+print(jsonauto)
+
+with open("auto.json","w",encoding="utf-8") as f:
+    f.write(jsonauto)
+
+with open("auto.json","r",encoding="utf-8") as f:
+    auto_dict = json.load(f)
+
+print(auto_dict)
+print(type(auto_dict))
+
