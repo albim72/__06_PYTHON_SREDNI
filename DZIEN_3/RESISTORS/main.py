2ced38fcb4cd6398d9b6d71f2db8e3a457bffae3
albim72 <marcin.albiniak72@gmail.com>
2023-06-28 14:14:18 +0200

    Update main.py

--- DZIEN_3/RESISTORS/main.py
+++ DZIEN_3/RESISTORS/main.py
@@ -1,5 +1,7 @@
 from oldresistor import OldResistor
 from resistor import Resistor
+from voltage import VoltageResistance
+from bounded import BoundedResistance
 
 
 print("__________________________________________")
@@ -21,3 +23,21 @@ print(f'układ elektryczny:\n'
       f'oporność: {r1.ohms} om\n'
       f'napięcie prądu: {r1.voltage} V\n'
       f'natężenie prądu: {r1.current} A\n')
+
+r2 = VoltageResistance(1.2E3)
+print(f"_______________ klasa {r2.__class__.__name__} ________________")
+
+print(f'natężenie początkowe prądu: {r2.current} A')
+r2.voltage = 48
+print(f'napięcie  prądu: {r2.voltage} V')
+print(f'natężenie  prądu: {r2.current} A')
+
+r3 = BoundedResistance(1.1E2)
+print(f"_______________ klasa {r3.__class__.__name__} ________________")
+try:
+      print(f'oporność pierwotna układu: {r3.ohms}')
+      r3.ohms = 15
+      print(f'oporność wtórna układu: {r3.ohms}')
+except ValueError as d:
+      print(d)
+
